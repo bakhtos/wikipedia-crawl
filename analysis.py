b695b222285011a039160e47d0da7e18b06d4d60
bakhtos <alexander.bakhtin@tuni.fi>
2022-02-21 18:17:50 +0200

    add: comments about pruning

--- analysis.py
+++ analysis.py
@@ -5,16 +5,33 @@ import pickle
 import re
 
 def prune_categories(categories, prune_dates=False):
-    to_return = dict()
+    """ Remove wikipedia meta-categories from pages' categories.
+    Parameters:
+    dict categories: dictionary of the form str -> set(str) containing
+        wikipedia categories for each pages (pages are keys)
+    bool prune_dates: if True, prune also categories related to dates.
+        (default False)
+    Return:
+    dict of the form str -> set(str), similar to input but with categories pruned
+    '''
+
+    to_return = dict() # New dict for pruned categories
+
+    # Regexes needed to prune
     prune = re.compile(r"[Aa]rticle|[Pp]ages|Wiki|Use \w* dates|Use .*English|[Tt]emplate")
     dates = re.compile(r"\d\d\d\d|century")
+    
+    # Iterate over data
     for page, cats in categories.items():
         new_cats = set()
         for cat in cats:
+            # Skip category if macted by pruning regexes
             if prune.search(cat): continue
             if prune_dates and dates.search(cat): continue
             else: new_cats.add(cat) 
+        # Save pruned categories
         to_return[page] = new_cats
+
     return to_return
 
 
@@ -27,4 +44,5 @@ if __name__ == '__main__':
 
     # Initialize the graph
     G = nx.DiGraph(links)
+    # Prune categories
     cats = prune_categories(categories, True)
