14bbe6a8469b454cde2dd3d02cb4fbbeb065edde
Alexander Bakhtin <alexander.bakhtin@tuni.fi>
2022-04-06 19:14:15 +0300

    remove: own random graph generators

--- analysis.py
+++ analysis.py
@@ -79,56 +79,6 @@ def make_random_as(G):
     BA_graph = nx.barabasi_albert_graph(n, m, seed=42)
     return ER_graph, BA_graph
 
-def WS_model(N, k, p):
-    G = nx.Graph()
-    # Add N nodes from 0 to N-1
-    G.add_nodes_from(range(N))
-    # for each node
-    for i in range(N):
-        # for each distance from node (1 to k/2)
-        for j in range(1,k//2+1):
-            # node 'to the left of i', accounting for cycling below 0
-            v1 = i-j if i-j>=0 else N+i-j
-            # node 'to the right of i', accouting for cycling above N
-            v2 = i+j if i+j<N else i+j-N
-            # Add the two edges
-            G.add_edge(i, v1)
-            G.add_edge(i, v2)
-    # New graph for storing rewired edges
-    G2 = nx.Graph()
-    # Iterate over edges
-    for edge in G.edges:
-        # with probability p...
-        if random.random()<p:
-            # choose a random end of the edge
-            node = random.choice(edge)
-            # make a list of all nodes and remove the chosen node
-            nodes = list(range(N))
-            nodes.remove(node)
-            # randomly choose another node from available ones
-            node2 = random.choice(nodes)
-            # this is the new edge
-            edge = (node, node2)
-        # Add an edge (either the same or rewired one) to graph
-        G2.add_edge(*edge)
-    return G2
-
-def BA_model(N, m):
-    # Make a complete graph
-    G = nx.complete_graph(m)
-    # Add remaining nodes
-    for i in range(m,N):
-        # Create a sorted list of nodes
-        nodes = sorted(G.nodes)
-        # Create a sorted (by node) list of degrees
-        degrees = [val for key,val in sorted(G.degree)]
-        # if we started we one node (m=1), its degree is 0,
-        # but the probability to connect should be 1
-        if degrees == [0]: degrees=[1]
-        js = random.choices(nodes, degrees, k=m)
-        for j in js: G.add_edge(i,j)
-    return G
-
 if __name__ == '__main__':
     # Load data from files
     with open('categories_clean.pickle', 'rb') as file:
